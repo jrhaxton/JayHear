581bb9c303c8cc0faa7e82628014d5814be05b68
Josh Haxton <haxtonjr@gmail.com>
2021-11-08 23:03:05 -0600

    Added in input file button, started header, and created login/user components

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InterfaceComponent } from './interface/interface.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InterfaceComponent,
+    HeaderComponent,
+    LoginComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
