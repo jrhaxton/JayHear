0bb37a324f13e3aa9dc2eeea813aebe78f8a4ac7
Josh Haxton <haxtonjr@gmail.com>
2021-11-13 21:29:14 -0600

    Added in save and download buttons and routing to login component

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { InterfaceComponent } from './interface/interface.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'home', component: InterfaceComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'header', component: HeaderComponent }
+  { path: 'header', component: HeaderComponent },
+  { path: 'login', component: LoginComponent}
 
 ];
 
