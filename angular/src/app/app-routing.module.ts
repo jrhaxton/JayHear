da8199ef8b7b870eea230a694dd1c3f4411b9f13
Josh Haxton <haxtonjr@gmail.com>
2022-02-17 23:49:06 -0600

    User management setup

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -4,11 +4,23 @@ import { HeaderComponent } from './header/header.component';
 import { InterfaceComponent } from './interface/interface.component';
 import { LoginComponent } from './login/login.component';
 
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ForgotComponent } from './components/forgot/forgot.component';
+import { VerifyComponent } from './components/verify/verify.component';
+import { AuthGuard } from "./shared/guard/auth.guard";
+
 const routes: Routes = [
   { path: 'home', component: InterfaceComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'header', component: HeaderComponent },
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent},
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'register-user', component: SignUpComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'forgot', component: ForgotComponent },
+  { path: 'verify', component: VerifyComponent }
 
 ];
 
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from '../environments/environment';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InterfaceComponent } from './interface/interface.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { UserComponent } from './user/user.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { VerifyComponent } from './components/verify/verify.component';
+import { ForgotComponent } from './components/forgot/forgot.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+import { AuthService } from "./shared/service/auth.service";
 
 @NgModule({
   declarations: [
@@ -14,13 +26,21 @@ import { UserComponent } from './user/user.component';
     InterfaceComponent,
     HeaderComponent,
     LoginComponent,
-    UserComponent
+    UserComponent,
+    SignInComponent,
+    SignUpComponent,
+    VerifyComponent,
+    ForgotComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
